c00a8595d980986ea3bb301afd9881e0fd5054ba
michael <chme@live.ru>
2024-02-06 16:32:13 +0700

    error code

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -9,25 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var firstVariableTextLabel: UILabel!
+    
+    @IBOutlet weak var secondVariableTextLabel: UILabel!
     
     @IBOutlet weak var firstVariableTextField: UITextField!
     
-    @IBOutlet weak var operationTypeSelectionButton: UIButton!
-    
-    @IBOutlet weak var resultButton: UIButton!
-    
     @IBOutlet weak var secondVariableTextField: UITextField!
     
+    @IBOutlet weak var operationTypeTextLabel: UILabel!
+    
+    @IBOutlet weak var operationTypeTextField: UITextField!
+    
+    @IBOutlet weak var resultButton: UIButton!
+
+    @IBOutlet weak var resultTextLabel: UILabel!
+    
     @IBOutlet weak var resultTextField: UITextField!
-    
-    @IBOutlet weak var sumOperationCommand: UICommand!
-    
-    @IBOutlet weak var substractOperationCommand: UICommand!
-    
-    @IBOutlet weak var divideOperationCommand: UICommand!
-    
-    @IBOutlet weak var multiplyOperationCommand: UICommand!
     
     var firstVariable: String = ""
     
@@ -35,18 +33,18 @@ class ViewController: UIViewController {
     
     var operationType = OperationType.sum
     
-
+    var mathOperation = MathOperation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello world!")
+        firstVariableTextLabel.text = "First Variable"
+        secondVariableTextLabel.text = "Second Variable"
         firstVariableTextField.text = "0"
         secondVariableTextField.text = "0"
+        operationTypeTextLabel.text = "Operation Type (+, -, /, *)"
+        resultTextLabel.text = "Result"
         resultTextField.text = "0"
-        sumOperationCommand.title = "+"
-        substractOperationCommand.title = "-"
-        divideOperationCommand.title = "/"
-        multiplyOperationCommand.title = "*"
         
     }
     
@@ -64,8 +62,33 @@ class ViewController: UIViewController {
         secondVariable = secondVariableTextField.text ?? "0"
     }
     
+    @IBAction func onEditingDidEnd(_ sender: Any) {
+        switch operationTypeTextField.text {
+        case "+":
+            operationType = .sum
+        case "-":
+            operationType = .substract
+        case "/":
+            operationType = .divide
+        case "*":
+            operationType = .multiply
+        default:
+            operationTypeTextLabel.text = "Error"
+        }
+    }
     @IBAction func onResultButtonTouchUpInside(_ sender: Any) {
-        
+        var a: Double = Double(firstVariable) ?? 0
+        var b: Double = Double(secondVariable) ?? 0
+        switch operationType {
+        case .sum:
+            resultTextField.text = String(mathOperation.sum(a, b))
+        case .substract:
+            resultTextField.text = String(mathOperation.substract(a, b))
+        case .divide:
+            resultTextField.text = String(mathOperation.divide(a, b))
+        case .multiply:
+            resultTextField.text = String(mathOperation.multiply(a, b))
+        }
     }
     
 }
